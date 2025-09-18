7470ade77f31f6a9a7b3d44a317d78ab88b5ae7f
Hazrat Ali <iloveyii@ayahoo.com>
2025-09-18 09:04:54 +0200

    init

---
+++ src/main/java/com/booking/cottage/CottageApplication.java
@@ -0,0 +1,13 @@
+package com.booking.cottage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CottageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CottageApplication.class, args);
+	}
+
+}
