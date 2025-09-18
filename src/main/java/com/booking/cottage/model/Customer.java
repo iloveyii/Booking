afb0bce7e00a11015aa7ef10f508f5a6cc47488c
Hazrat Ali <iloveyii@ayahoo.com>
2025-09-18 09:33:13 +0200

    feat: add lombok

--- src/main/java/com/booking/cottage/model/Customer.java
+++ src/main/java/com/booking/cottage/model/Customer.java
@@ -1,8 +1,13 @@
 package com.booking.cottage.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 
 @Entity
+@Getter
+@Setter
 public class Customer {
 
     @Id
@@ -12,23 +17,5 @@ public class Customer {
     private String name;
     private String email;
     private String phone;
-
-    public Customer() {}
-
-    public Customer(String name, String email, String phone) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    // getters and setters
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public String getPhone() { return phone; }
-
-    public void setName(String name) { this.name = name; }
-    public void setEmail(String email) { this.email = email; }
-    public void setPhone(String phone) { this.phone = phone; }
 }
 
